d539eb0cc969427659c458c512e3841fa2d2094e
Lenny <LennyPhoenixC@gmail.com>
2021-01-03 16:51:09 +0000

    🛠 Refactored util functions.

--- src/util.c
+++ src/util.c
@@ -1,60 +1,67 @@
 #include "util.h"
 
+godot_string get_script_path(godot_string *p_class_name,
+                             Library *p_lib)
+{
+    // Get the .gdnlib path
+    char *gdnlib_path;
+    int gdnlib_path_size;
+    {
+        godot_method_bind *mb_get_path = p_lib->api->godot_method_bind_get_method("Resource", "get_path");
+
+        godot_variant_call_error error;
+        godot_variant ret = p_lib->api->godot_method_bind_call(mb_get_path, (godot_object *)p_lib->gdnlib, NULL, 0, &error);
+
+        godot_string gdnlib_path_string = p_lib->api->godot_variant_as_string(&ret);
+        godot_char_string gdnlib_path_char_string = p_lib->api->godot_string_utf8(&gdnlib_path_string);
+
+        gdnlib_path = (char *)p_lib->api->godot_char_string_get_data(&gdnlib_path_char_string);
+        gdnlib_path_size = p_lib->api->godot_char_string_length(&gdnlib_path_char_string);
+    }
+
+    // Get script name
+    char *script_name;
+    int script_name_size;
+    {
+        godot_string script_name_string = p_lib->api->godot_string_camelcase_to_underscore_lowercased(p_class_name);
+        godot_char_string script_name_char_string = p_lib->api->godot_string_utf8(&script_name_string);
+
+        script_name = (char *)p_lib->api->godot_char_string_get_data(&script_name_char_string);
+        script_name_size = p_lib->api->godot_char_string_length(&script_name_char_string);
+    }
+
+    // Get folder of script
+    int last_slash_char;
+    for (int i = 0; i < gdnlib_path_size; i++)
+    {
+        if (gdnlib_path[i] == '/')
+        {
+            last_slash_char = i + 1;
+        }
+    }
+
+    // Join the strings together
+    char *script_path = calloc(last_slash_char + script_name_size + 6, sizeof(char));
+    memcpy(script_path, gdnlib_path, last_slash_char * sizeof(char));
+    memcpy(script_path + last_slash_char, script_name, script_name_size * sizeof(char));
+    memcpy(script_path + last_slash_char + script_name_size, ".gdns", 5 * sizeof(char));
+
+    godot_string script_path_string = p_lib->api->godot_string_chars_to_utf8(script_path);
+
+    free(script_path);
+
+    return script_path_string;
+}
+
 godot_object *instantiate_custom_class(const char *p_class_name, const char *p_base,
                                        Library *p_lib)
 {
-    godot_string class_name;
+    godot_string class_name = p_lib->api->godot_string_chars_to_utf8(p_class_name);
 
-    p_lib->api->godot_string_new(&class_name);
-    p_lib->api->godot_string_parse_utf8(&class_name, p_class_name);
-
-    godot_object *script;
     // Get the script object
+    godot_object *script;
     {
-        godot_string script_path_string;
-
-        // Get the .gdns path
-        {
-            godot_string path_string;
-            { // Get the .gdnlib path
-                godot_method_bind *mb_get_path = p_lib->api->godot_method_bind_get_method("Resource", "get_path");
-
-                godot_variant_call_error error;
-                godot_variant ret = p_lib->api->godot_method_bind_call(mb_get_path, (godot_object *)p_lib->gdnlib, NULL, 0, &error);
-
-                path_string = p_lib->api->godot_variant_as_string(&ret);
-            }
-            godot_char_string path_char_string = p_lib->api->godot_string_utf8(&path_string);
-
-            const char *path = p_lib->api->godot_char_string_get_data(&path_char_string);
-            int path_size = p_lib->api->godot_char_string_length(&path_char_string);
-
-            // Get script name
-            godot_string script_name_string = p_lib->api->godot_string_camelcase_to_underscore_lowercased(&class_name);
-            godot_char_string script_name_char_string = p_lib->api->godot_string_utf8(&script_name_string);
-            const char *script_name = p_lib->api->godot_char_string_get_data(&script_name_char_string);
-            int script_name_size = p_lib->api->godot_char_string_length(&script_name_char_string);
-
-            // Get folder of script
-            int last_slash_char;
-            for (int i = 0; i < path_size; i++)
-            {
-                if (path[i] == '/')
-                {
-                    last_slash_char = i + 1;
-                }
-            }
-
-            // Join the strings together
-            char *script_path = calloc(last_slash_char + script_name_size + 6, sizeof(char));
-            memcpy(script_path, path, sizeof(char) * last_slash_char);
-            memcpy(script_path + last_slash_char, script_name, sizeof(char) * script_name_size);
-            memcpy(script_path + last_slash_char + script_name_size, ".gdns", sizeof(char) * 5);
-
-            script_path_string = p_lib->api->godot_string_chars_to_utf8(script_path);
-
-            free(script_path);
-        }
+        godot_string script_path_string = get_script_path(&class_name, p_lib);
 
         // Load the script
         godot_object *resource_loader = p_lib->api->godot_global_get_singleton("ResourceLoader");
@@ -62,65 +69,61 @@ godot_object *instantiate_custom_class(const char *p_class_name, const char *p_b
         godot_variant resource_loader_variant;
         p_lib->api->godot_variant_new_object(&resource_loader_variant, resource_loader);
 
-        godot_string method_name = p_lib->api->godot_string_chars_to_utf8("load");
-
         godot_variant path_variant;
         p_lib->api->godot_variant_new_string(&path_variant, &script_path_string);
 
-        godot_variant *args[] = {&path_variant};
+        godot_variant script_variant;
+        {
+            godot_variant *args[] = {&path_variant};
 
-        godot_variant_call_error error;
-        godot_variant script_variant = p_lib->api->godot_variant_call(&resource_loader_variant,
-                                                                      &method_name,
-                                                                      args, 1,
-                                                                      &error);
+            godot_string method_name = p_lib->api->godot_string_chars_to_utf8("load");
+
+            godot_variant_call_error error;
+            script_variant = p_lib->api->godot_variant_call(&resource_loader_variant,
+                                                            &method_name,
+                                                            args, 1,
+                                                            &error);
+        }
 
         script = p_lib->api->godot_variant_as_object(&script_variant);
     }
 
     // Construct the base object
     godot_class_constructor base_constructor = p_lib->api->godot_get_class_constructor(p_base);
-    godot_method_bind *mb_set_script = p_lib->api->godot_method_bind_get_method("Object", "set_script");
-    godot_object *base_obj = base_constructor();
+    godot_object *object = base_constructor();
+
     // Add the script
     {
+        godot_method_bind *mb_set_script = p_lib->api->godot_method_bind_get_method("Object", "set_script");
+
         const void *args[] = {script};
-        p_lib->api->godot_method_bind_ptrcall(mb_set_script, base_obj, args, NULL);
+
+        p_lib->api->godot_method_bind_ptrcall(mb_set_script, object, args, NULL);
     }
 
-    // p_lib->api->godot_object_destroy(script);
-    p_lib->api->godot_string_destroy(&class_name);
-
-    return base_obj;
+    return object;
 }
 
 godot_variant_call_error object_emit_signal(godot_object *p_object,
-                                            const char *p_signal_name,
-                                            int p_num_args, godot_variant *p_args,
+                                            godot_string *p_signal_name,
+                                            int p_num_args, godot_variant **p_args,
                                             Library *p_lib)
 {
     godot_variant variant;
     p_lib->api->godot_variant_new_object(&variant, p_object);
 
-    godot_string method_name;
-    p_lib->api->godot_string_new(&method_name);
-    p_lib->api->godot_string_parse_utf8(&method_name, "emit_signal");
+    godot_string method_name = p_lib->api->godot_string_chars_to_utf8("emit_signal");
 
     godot_variant **args = calloc(p_num_args + 1, sizeof(godot_variant *));
 
-    godot_string signal_name;
-    p_lib->api->godot_string_new(&signal_name);
-    p_lib->api->godot_string_parse_utf8(&signal_name, p_signal_name);
-
     godot_variant variant_signal_name;
-    p_lib->api->godot_variant_new_string(&variant_signal_name, &signal_name);
-    p_lib->api->godot_string_destroy(&signal_name);
+    p_lib->api->godot_variant_new_string(&variant_signal_name, p_signal_name);
 
     args[0] = &variant_signal_name;
 
     for (int i = 0; i < p_num_args; i++)
     {
-        args[i + 1] = &p_args[i];
+        args[i + 1] = p_args[i];
     }
 
     godot_variant_call_error error;
--- src/util.h
+++ src/util.h
@@ -6,12 +6,15 @@
 #include <stdbool.h>
 #include <gdnative_api_struct.gen.h>
 
+godot_string get_script_path(godot_string *name,
+                             struct Library *p_lib);
+
 godot_object *instantiate_custom_class(const char *p_class_name, const char *p_base,
                                        struct Library *p_lib);
 
 godot_variant_call_error object_emit_signal(godot_object *p_object,
-                                            const char *p_signal_name,
-                                            int p_num_args, godot_variant *p_args,
+                                            godot_string *p_signal_name,
+                                            int p_num_args, godot_variant **p_args,
                                             struct Library *p_lib);
 
 #endif
