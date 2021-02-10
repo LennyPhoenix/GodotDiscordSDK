6e2b4029996a0bb5306b897ff9a471b6ace49a3d
Lenny <LennyPhoenixC@gmail.com>
2021-02-10 16:04:26 +0000

    🛠 Move Manage cleanup.

--- src/core.c
+++ src/core.c
@@ -14,21 +14,21 @@ GDCALLINGCONV void *core_constructor(godot_object *p_instance, Library *p_lib)
 GDCALLINGCONV void core_destructor(godot_object *p_instance, Library *p_lib,
                                    Core *p_core)
 {
-    if (p_core->internal)
-    {
-        p_core->internal->destroy(p_core->internal);
-        p_lib->api->godot_free(p_core->user_events);
-    }
-
-    if (p_core->hook_data)
-        p_lib->api->godot_free(p_core->hook_data);
-
     if (p_core->users)
         godot_unreference(p_core->users, p_lib);
     if (p_core->images)
         godot_unreference(p_core->images, p_lib);
     if (p_core->activities)
         godot_unreference(p_core->activities, p_lib);
+
+    if (p_core->hook_data)
+        p_lib->api->godot_free(p_core->hook_data);
+
+    if (p_core->internal)
+    {
+        p_core->internal->destroy(p_core->internal);
+        p_lib->api->godot_free(p_core->user_events);
+    }
 
     p_lib->api->godot_free(p_core);
 }
