c64a074311deb39c329ffb83dd0a36e5503bc9bd
Lenny <LennyPhoenixC@gmail.com>
2021-02-19 12:09:16 +0000

    🛠 Remove discord_game_sdk.h from the repository.

--- setup.bat
+++ setup.bat
@@ -1,9 +1,16 @@
 @echo off
+
+echo [91mBy running this script you agree to the Discord Developers ToS.[37m 
+echo.
+
+echo [32mDownloading the Discord Game SDK...[37m 
 curl.exe --output discord_game_sdk.zip --url https://dl-game-sdk.discordapp.net/2.5.6/discord_game_sdk.zip
 powershell.exe "Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::ExtractToDirectory('discord_game_sdk.zip', 'dsdk');"
 
 mkdir demo\bin\windows-32
 mkdir demo\bin\windows-64
+
+copy dsdk\c\discord_game_sdk.h src\discord_game_sdk.h 
 
 copy dsdk\lib\x86\discord_game_sdk.dll demo\bin\windows-32\discord_game_sdk.dll
 copy dsdk\lib\x86\discord_game_sdk.dll.lib lib\discord_game_sdk.32.lib
--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+echo -e "By running this script you agree to the Discord Developers ToS.\n"
+
+echo Downloading the Discord Game SDK...
 wget https://dl-game-sdk.discordapp.net/2.5.6/discord_game_sdk.zip
 unzip discord_game_sdk -d dsdk
 
@@ -5,6 +8,8 @@ mkdir demo/bin/linux-32
 mkdir demo/bin/linux-64
 mkdir demo/bin/osx-32
 mkdir demo/bin/osx-64
+
+cp dsdk/c/discord_game_sdk.h src/discord_game_sdk.h 
 
 cp dsdk/lib/x86/discord_game_sdk.dll.lib lib/discord_game_sdk.32.lib
 cp dsdk/lib/x86_64/discord_game_sdk.dll.lib lib/discord_game_sdk.64.lib
