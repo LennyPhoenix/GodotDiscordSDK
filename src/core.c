80ace208703d8405f63e3e767d629d95ff10fd2f
LennyPhoenix <LennyPhoenixC@gmail.com>
2021-03-06 19:11:18 +0000

    🛠 Fix issue with Managers not being freed

--- src/core.c
+++ src/core.c
@@ -19,12 +19,12 @@ GDCALLINGCONV void *core_constructor(godot_object *p_instance, Library *p_lib)
 GDCALLINGCONV void core_destructor(godot_object *p_instance, Library *p_lib,
                                    Core *p_core)
 {
-    if (p_lib->core_1_1_api->godot_is_instance_valid(p_core->users))
-        godot_unreference(p_core->users, p_lib);
-    if (p_lib->core_1_1_api->godot_is_instance_valid(p_core->images))
-        godot_unreference(p_core->images, p_lib);
-    if (p_lib->core_1_1_api->godot_is_instance_valid(p_core->activities))
-        godot_unreference(p_core->activities, p_lib);
+    if (p_core->users)
+        godot_unreference(p_core->users->object, p_lib);
+    if (p_core->images)
+        godot_unreference(p_core->images->object, p_lib);
+    if (p_core->activities)
+        godot_unreference(p_core->activities->object, p_lib);
 
     if (p_core->hook_data)
     {
