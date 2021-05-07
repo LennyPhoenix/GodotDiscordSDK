acc0c5b4a22087010ffc5bd381cbba4904a571ae
LennyPhoenix <LennyPhoenixC@gmail.com>
2021-05-07 21:04:53 +0100

    🛠 Add flush to NetworkManager

--- src/networking.c
+++ src/networking.c
@@ -42,6 +42,29 @@ godot_variant network_manager_get_peer_id(godot_object *p_instance, void *p_meth
     else
     {
         PRINT_ERROR("Invalid number of arguments for \"get_peer_id()\" call. Expected 0.", lib);
+        lib->core_api->godot_variant_new_nil(&result_variant);
+    }
+
+    return result_variant;
+}
+
+godot_variant network_manager_flush(godot_object *p_instance, void *p_method_data, void *p_user_data, int p_num_args,
+                                    godot_variant **p_args)
+{
+    Library *lib                    = p_method_data;
+    NetworkManager *network_manager = p_user_data;
+
+    godot_variant result_variant;
+
+    if (p_num_args == 0)
+    {
+        enum EDiscordResult result = network_manager->internal->flush(network_manager->internal);
+        lib->core_api->godot_variant_new_int(&result_variant, result);
+    }
+    else
+    {
+        PRINT_ERROR("Invalid number of arguments for \"flush()\" call. Expected 0.", lib);
+        lib->core_api->godot_variant_new_nil(&result_variant);
     }
 
     return result_variant;
@@ -75,6 +98,15 @@ void register_network_manager(void *p_handle, Library *p_lib)
 
             p_lib->nativescript_api->godot_nativescript_register_method(p_handle, "NetworkManager", "get_peer_id",
                                                                         attributes, method);
+        }
+        // Flush
+        {
+            memset(&method, 0, sizeof(godot_instance_method));
+            method.method      = network_manager_flush;
+            method.method_data = p_lib;
+
+            p_lib->nativescript_api->godot_nativescript_register_method(p_handle, "NetworkManager", "flush", attributes,
+                                                                        method);
         }
     }
 }
