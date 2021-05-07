9d6f934f88260855b301fb9431bb42820bf24b61
LennyPhoenix <LennyPhoenixC@gmail.com>
2021-05-07 20:32:01 +0100

    🛠 Implement get_peer_id for NetworkManager

--- src/networking.c
+++ src/networking.c
@@ -24,6 +24,29 @@ GDCALLINGCONV void network_manager_destructor(godot_object *p_instance, void *p_
     lib->core_api->godot_free(network_manager);
 }
 
+godot_variant network_manager_get_peer_id(godot_object *p_instance, void *p_method_data, void *p_user_data,
+                                          int p_num_args, godot_variant **p_args)
+{
+    Library *lib                    = p_method_data;
+    NetworkManager *network_manager = p_user_data;
+
+    godot_variant result_variant;
+
+    if (p_num_args == 0)
+    {
+        uint64_t peer_id;
+        network_manager->internal->get_peer_id(network_manager->internal, &peer_id);
+
+        lib->core_api->godot_variant_new_uint(&result_variant, peer_id);
+    }
+    else
+    {
+        PRINT_ERROR("Invalid number of arguments for \"get_peer_id()\" call. Expected 0.", lib);
+    }
+
+    return result_variant;
+}
+
 void register_network_manager(void *p_handle, Library *p_lib)
 {
     godot_instance_create_func constructor;
@@ -38,4 +61,20 @@ void register_network_manager(void *p_handle, Library *p_lib)
 
     p_lib->nativescript_api->godot_nativescript_register_class(p_handle, "NetworkManager", "Object", constructor,
                                                                destructor);
+
+    // Methods
+    {
+        godot_instance_method method;
+        godot_method_attributes attributes = {GODOT_METHOD_RPC_MODE_DISABLED};
+
+        // Get Peer ID
+        {
+            memset(&method, 0, sizeof(godot_instance_method));
+            method.method      = network_manager_get_peer_id;
+            method.method_data = p_lib;
+
+            p_lib->nativescript_api->godot_nativescript_register_method(p_handle, "NetworkManager", "get_peer_id",
+                                                                        attributes, method);
+        }
+    }
 }
